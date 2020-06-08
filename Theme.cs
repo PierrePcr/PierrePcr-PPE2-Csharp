720507aeb082c9e2954891030774f90de83607f4
PierrePcr <56682229+PierrePcr@users.noreply.github.com>
2020-06-08 13:27:53 +0200

    Add files via upload

---
+++ Theme.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssisesSportLorrain
+{
+    public class Theme
+    {
+        private int idTheme;
+        private string nomTheme;
+        private int idAtelier;
+
+        public Theme(int idTheme, string nomTheme, int idAtelier)
+        {
+            this.idTheme = idTheme;
+            this.nomTheme = nomTheme;
+            this.idAtelier = idAtelier;
+        }
+
+        #region Accesseurs
+        public int IdTheme
+        {
+            get
+            {
+                return idTheme;
+            }
+
+            set
+            {
+                idTheme = value;
+            }
+        }
+
+        public string NomTheme
+        {
+            get
+            {
+                return nomTheme;
+            }
+
+            set
+            {
+                nomTheme = value;
+            }
+        }
+
+        public int IdAtelier
+        {
+            get
+            {
+                return idAtelier;
+            }
+
+            set
+            {
+                idAtelier = value;
+            }
+        }
+        #endregion
+
+        #region Méthodes d'appel au DAO métier
+
+        // Méthode statique qui retourne l'ensemble des Themes sous forme de List
+        public static List<Theme> listeThemes()
+        {
+            return DAOTheme.getAllThemes();
+        }
+
+        // Fait créer le Theme (objet courant) dans la BDD
+        public void ajouterTheme()
+        {
+            DAOTheme.creerTheme(this);
+        }
+
+        // Fait modifier le Theme (objet courant) dans la BDD
+        public void modifierTheme(int idTheme, string nomTheme, int idAtelier)
+        {
+            this.idTheme = idTheme + 1;
+            this.nomTheme = nomTheme;
+            this.idAtelier = idAtelier;
+            DAOTheme.modifierTheme(this);
+        }
+
+        // Supprimer un theme
+        public void supprimerTheme()
+        {
+            DAOTheme.supprimerTheme(this);
+
+        }
+        #endregion
+    }
+}
---
+++ Utilisateur.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssisesSportLorrain
+{
+    class Utilisateur
+    {
+        private string loginUtilisateur;
+        private string mdpUtilisateur;
+
+
+
+ 
+
+        public string LoginUtilisateur
+        {
+            get { return loginUtilisateur; }
+            set { loginUtilisateur = value; }
+        }
+
+        public string MdpUtilisateur
+        {
+            get { return mdpUtilisateur; }
+            set { mdpUtilisateur = value; }
+        }
+
+        
+
+        public Utilisateur(string loginUtilisateur, string mdpUtilisateur)
+        {
+            this.loginUtilisateur = loginUtilisateur;
+            this.mdpUtilisateur = mdpUtilisateur;
+        }
+
+
+        public static List<Utilisateur> listeUtilisateurs()
+        {
+            return DAOUtilisateur.getAllUtilisateurs();
+        }
+
+
+
+        public static Utilisateur lUtilisateur()
+        {
+            return DAOUtilisateur.getlUtilisateur();
+        }
+
+        /*
+        public void connexionUtilisateur()
+        {
+            DAOUtilisateur.getAllUtilisateurs(this);
+        }*/
+
+
+    }
+}
